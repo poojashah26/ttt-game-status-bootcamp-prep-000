cf6daeea15c2420068ca47ebc80154348ce15dc4
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-10-05 10:45:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,32 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]
+
+def won?(board)
+end
+
+def won?(board)
+  WIN_COMBINATIONS.find do |win_combination|
+    board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] && position_taken?(board, win_combination[0])
+  end
+end
+
+def full?(board)
+  board.all? do |index|
+    index == "X" || index == "O"  
+  end
+end
+
+def draw?(board)
+  
